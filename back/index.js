ad933f8de48b4330d720204f2a1ac20e217c3bf0
soorajkumar07 <soorajkumar1493a@gmail.com>
2023-03-03 13:38:40 +0530

    first

---
+++ back/index.js
@@ -0,0 +1,83 @@
+const express = require('express');
+ const app=express();
+
+
+const mysql = require('mysql');
+
+const cors=require('cors');
+
+
+app.use(cors())
+
+app.use(express.json());
+
+
+// database connection
+
+
+const db=mysql.createConnection({
+
+ user: "fbdro",
+  host: "10.125.20.10",
+  password: "fbdro",
+  database: "his_db_22022023",
+})
+
+
+// putting the information
+
+
+app.post("/enter",(req, res) => {
+
+    const MRD=req.body.MRD   
+    let query="INSERT INTO bill (patient_id) VALUE(?)"
+
+    db.query(query,[MRDNum],(err,result)=>{
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.send("result entered")
+        }
+    })
+
+})
+
+
+// getting the data from the db
+
+app.get("/get",(req,res)=>{
+    let querty=`select ps.patient_id,b.visit_type,b.visit_code,ps.last_modified_by,hu.user_name,dm.description,ps.last_modified_date_time,ps.credit_limit,ps.credit_type,ps.pharmacy_material_credit_limit,ps.pharmacy_material_credit_type,ps.credit_amount from patient_account_summary ps
+    left join bill b on ps.patient_id=b.patient_id
+    left join his_user hu on ps.last_modified_by=hu.user_login
+    left join staff_master sm on hu.user_staff_id=sm.id
+    left join department_master dm on sm.dept_id= dm.id
+    
+    where ps.patient_id='${req.query.MRD}' and visit_type='ip' and visit_code like(select visit_code from his_db_22022023.bill where patient_id='${req.query.MRD}' and visit_type like 'ip%'  order by visit_code desc limit 1)
+    order by ps.visit_id desc`
+
+    db.query(querty,(err,result)=>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+        }
+    })
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(9000,()=>{
+    console.log("listening ")
+})
---
+++ front/src/App.js
@@ -0,0 +1,123 @@
+import { React,useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import moment from 'moment'
+function App() {
+
+  const [list,setlist]=useState([])
+  const[MRDNum,setMRDNum]=useState()
+
+
+  const getBill=() =>{
+
+    let params={
+      "MRD":MRDNum
+    }
+axios.get("http://localhost:9000/get",{params}).then((Responce) => {
+
+
+setlist(Responce.data)  
+// console.log(Response)
+console.log ("got the data")
+
+
+  
+}).catch((err) => {
+  console.log(err)
+});
+
+
+
+
+  }
+
+
+
+
+  return (
+    <div className="App">
+      
+      <div className="main3">
+
+      <h3>Credit Limit</h3>
+
+      <label>MRD: </label>
+
+      <input className='text'
+        type={"text"}
+        onChange={(event) => {
+          setMRDNum(event.target.value);
+          // console.log(event.target.value)
+        }}
+      />
+
+
+      <button className="btn" onClick={getBill}>
+        get details
+      </button>
+    </div>
+
+      <table className='table' border={5} cellPadding={10}>
+        <thead>
+        <tr  className='tableraw'>
+          <th>Patient_id</th>
+        <th>Visit_Type</th>
+        <th>Visit_Code</th>
+        <th>Last_Modified_By</th>
+        <th>Staff_Display_Name</th>
+        <th>Description</th>
+        <th>Last_Modified_Date_Time</th>
+        <th>Cedit_Amount</th>
+        <th>Credit_Limit</th>
+        <th>Credit_Type</th>
+        <th>Pharmacy_Material_Credit_Limit</th>
+        <th>Pharmacy_Material_Credit_Type</th>
+        </tr>
+        </thead>
+
+
+          <tbody>
+          {list.map((val)=>{
+
+            return(
+              <tr className='tabraw'>
+
+                <td>{val.patient_id}</td>
+
+                
+
+                <td>{val.visit_code}</td>
+
+                <td>{val.visit_type}</td>
+
+                <td>{val.last_modified_by}</td>
+
+                <td>{val.user_name}</td>
+
+                <td>{val.description}</td>
+
+                <td>{moment(val.last_modified_date_time).format('MMM Do YYYY, h:mm:ss a')}</td>
+
+                <td>{val.credit_amount}</td>
+
+                <td>{val.credit_limit}</td>
+
+                <td>{val.credit_type}</td>
+
+                <td>{val.pharmacy_material_credit_limit}</td>
+
+                <td>{val.pharmacy_material_credit_type}</td>
+              </tr>
+            )
+          })}
+          </tbody>
+
+       </table>
+
+
+
+</div>
+  );
+}
+
+export default App;
